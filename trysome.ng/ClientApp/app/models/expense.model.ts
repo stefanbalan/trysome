a0232487cc28e56ddcab9ff4afb14a3954ebe826
Stefan Balan <stefan.balan@gmail.com>
2018-07-31 18:44:17 +0300

    initial; dotnet angular spa template (ng 4.2, ts 2.4.1)

---
+++ trysome.ng/ClientApp/app/models/expense.model.ts
@@ -0,0 +1,11 @@
+﻿export class Expense {
+    constructor(
+        public amount: number = 0,
+        public category: CategoryRef | undefined) { }
+}
+
+export class CategoryRef {
+    constructor(
+        public id: number,
+        public name: string) { }
+}
