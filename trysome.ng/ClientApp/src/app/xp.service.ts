4bafd3b32f02d6ec298c0d1a9167642cb26667dd
Stefan Balan <stefan.balan@gmail.com>
2018-08-28 00:08:32 +0300

    xpservice

--- trysome.ng/ClientApp/src/app/xp.service.ts
+++ trysome.ng/ClientApp/src/app/xp.service.ts
@@ -1,7 +1,12 @@
 import { Expense } from './model/expense';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable } from 'rxjs/observable';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable()
 export class XpService {
@@ -14,12 +19,27 @@ export class XpService {
     })
   };
 
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    // return an observable with a user-facing error message
+    return throwError('Something bad happened; please try again later.');
+  }
+
   addExpense(expense: Expense): boolean {
-    // return this.http.post<Expense>('api/Expense', expense, httpOptions)
-    // .pipe(
-    //   catchError(this.handleError('addHero', hero))
-    // );
     console.log(expense);
+    const result = this.http
+      .post<Expense>('api/Expense', expense, this.httpOptions)
+      .pipe(catchError(this.handleError('addHero', expense)));
+
     return true;
   }
 }
