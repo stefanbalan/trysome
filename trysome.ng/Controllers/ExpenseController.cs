5ccdd482b512d6fe3beaa9b5f35ed3bc25959053
Balan Stefan <stefan.balan@gmail.com>
2018-11-21 09:50:08 +0200

    some work on the controller

--- trysome.ng/Controllers/ExpenseController.cs
+++ trysome.ng/Controllers/ExpenseController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using trysome.ng.Models;
 
 namespace trysome.ng.Controllers
@@ -6,6 +7,19 @@ namespace trysome.ng.Controllers
     [Route("api/[controller]")]
     public class ExpenseController : Controller
     {
+        [HttpPost("[action]")]
+        public Expense[] Get()
+        {
+            return new Expense[]
+            {
+                new Expense()
+                {
+                    Amount = 12,
+                    Category = "lunch", Date = DateTime.Now,
+                    Tags = "t1, t2"
+                }
+            };
+        }
 
         [HttpPost("[action]")]
         public void Post([FromBody] Expense expense)
