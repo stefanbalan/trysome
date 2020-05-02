e12f80063751c6cf884f6c1c6c713d7043b263cf
Eugen Stefan BALAN <x170375>
2020-05-02 23:54:05 +0300

    state, props, hooks, propTypes

--- ps-flux/src/components/App.js
+++ ps-flux/src/components/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
+import CoursesPage from "./CoursesPage";
 import Header from "./common/Header";
 
 function App() {
   function getPage() {
     const route = window.location.pathname;
-    if (route === "/about") {
-      return <AboutPage />;
-    } else {
-      return <HomePage />;
-    }
+    if (route === "/courses") return <CoursesPage />;
+    if (route === "/about") return <AboutPage />;
+    return <HomePage />;
   }
   return (
     <div className="container-fluid">
