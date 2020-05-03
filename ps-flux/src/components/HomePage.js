349421b3865ae22176195b08d62766ea4b63dfd3
Eugen Stefan BALAN <x170375>
2020-05-03 11:56:15 +0300

    navlinks

--- ps-flux/src/components/HomePage.js
+++ ps-flux/src/components/HomePage.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <div className="jumbotron">
       <h1>Pluralsight Administration</h1>
       <p>React, FLux and React-Router for responsive apps</p>
-      <a href="/about">About</a>
+      <Link to="about" className="btn btn-primary">
+        About
+      </Link>
     </div>
   );
 }
--- ps-flux/src/index.js
+++ ps-flux/src/index.js
@@ -20,5 +20,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-render(<App />, document.getElementById("root"));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
