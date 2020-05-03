50571b1a4263b4489c0e5e3878ddd33f82ff6c75
Eugen Stefan BALAN <x170375>
2020-05-03 17:22:28 +0300

    handle not found

--- ps-flux/src/components/App.js
+++ ps-flux/src/components/App.js
@@ -2,16 +2,21 @@ import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import CoursesPage from "./CoursesPage";
+import NotFoundPage from "./NotFoundPage";
 import Header from "./common/Header";
-import { Route } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 function App() {
   return (
     <div className="container-fluid">
       <Header />
-      <Route path="/" exact component={HomePage} />
-      <Route path="/courses" component={CoursesPage} />
-      <Route path="/about" component={AboutPage} />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Redirect from="/about-page" to="/about" />
+        <Route component={NotFoundPage} />
+      </Switch>
     </div>
   );
 }
