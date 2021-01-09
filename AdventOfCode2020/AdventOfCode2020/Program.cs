d67a67ddddfeeb71e06a95dfe50552bd05434eec
Eugen Stefan BALAN <x170375>
2021-01-09 21:24:21 +0200

    AoC day 1

---
+++ AdventOfCode2020/AdventOfCode2020/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Day1.Compute2();
+            Console.WriteLine(result);
+        }
+    }
+}
