fb4d66121aec048f6d35407e75d5996bb0a1d22b
Stefan Balan <stefan.balan@gmail.com>
2021-07-18 13:20:22 +0300

    Develop (#25)
    * Bump diff from 3.4.0 to 3.5.0 in /trysome.ng/ClientApp
    
    Bumps [diff](https://github.com/kpdecker/jsdiff) from 3.4.0 to 3.5.0.
    - [Release notes](https://github.com/kpdecker/jsdiff/releases)
    - [Changelog](https://github.com/kpdecker/jsdiff/blob/master/release-notes.md)
    - [Commits](https://github.com/kpdecker/jsdiff/compare/v3.4.0...v3.5.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * module 5
    
    * Bump jquery from 1.9.1 to 3.5.0 in /trysome.ng/ClientApp
    
    Bumps [jquery](https://github.com/jquery/jquery) from 1.9.1 to 3.5.0.
    - [Release notes](https://github.com/jquery/jquery/releases)
    - [Commits](https://github.com/jquery/jquery/compare/1.9.1...3.5.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * state, props, hooks, propTypes
    
    * navlinks
    
    * handle not found
    
    * link to manage course
    
    * Bump websocket-extensions from 0.1.3 to 0.1.4 in /trysome.ng/ClientApp
    
    Bumps [websocket-extensions](https://github.com/faye/websocket-extensions-node) from 0.1.3 to 0.1.4.
    - [Release notes](https://github.com/faye/websocket-extensions-node/releases)
    - [Changelog](https://github.com/faye/websocket-extensions-node/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/faye/websocket-extensions-node/compare/0.1.3...0.1.4)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump lodash from 4.17.15 to 4.17.19 in /trysome.ng/ClientApp
    
    Bumps [lodash](https://github.com/lodash/lodash) from 4.17.15 to 4.17.19.
    - [Release notes](https://github.com/lodash/lodash/releases)
    - [Commits](https://github.com/lodash/lodash/compare/4.17.15...4.17.19)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * esimaps
    
    * react with flux pluralsight
    
    * AoC day 1
    
    * AoC day 2
    
    * AoC day 3
    
    * AoC day 4
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Eugen Stefan BALAN <x170375>

---
+++ AdventOfCode2020/AdventOfCode2020/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var day = new Day4();
+            var result = day.Compute1();
+            Console.WriteLine(result);
+
+            result = day.Compute2();
+            Console.WriteLine(result);
+        }
+    }
+}
