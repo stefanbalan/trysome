cdd91bfa6393fafc769f7ba9d9380b7d3e68a46f
Eugen Stefan BALAN <x170375>
2021-11-10 21:00:19 +0200

    base entity

---
+++ ClashOfLogs/CoL.DB.mssql/Migrations/20211110185837_BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoL.DB.mssql.Migrations
+{
+    public partial class BaseEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ImportDate",
+                table: "Clans",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImportDate",
+                table: "Clans");
+        }
+    }
+}
