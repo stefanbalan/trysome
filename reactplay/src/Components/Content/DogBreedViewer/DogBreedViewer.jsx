821d72f8bfd6c55e34e22195f842b270c1823b28
Stefan Balan <stefan.balan@gmail.com>
2022-05-26 09:41:09 +0300

    react play initial

---
+++ reactplay/src/Components/Content/DogBreedViewer/DogBreedViewer.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const DogBreedViewer = () => {
+  return <div></div>;
+};
---
+++ reactplay/src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <p>Please give credit to:</p>
+      <a target="_blank" rel="noreferrer" href="https://dog.ceo/dog-api/documentation/">
+        Dog API
+      </a>
+    </footer>
+  );
+};
