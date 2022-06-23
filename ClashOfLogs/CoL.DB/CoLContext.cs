fb6804732f37deab796ff1aff5e4fd8078d6a649
Stefan Balan <stefan.balan@gmail.com>
2022-06-23 17:52:27 +0300

    wip

--- ClashOfLogs/CoL.DB/CoLContext.cs
+++ ClashOfLogs/CoL.DB/CoLContext.cs
@@ -20,7 +20,9 @@ namespace CoL.DB.mssql
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CoL;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CoL;" +
+                    "Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;" +
+                    "ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             }
         }
 
