ccb09ea3d5ee15849147566d48080bc148f660e0
Eugen Stefan BALAN <x170375>
2022-12-29 20:12:57 +0200

    AoC22 Day4

--- AdventOfCode2022/AdventOfCode2022/Day.cs
+++ AdventOfCode2022/AdventOfCode2022/Day.cs
@@ -1,21 +1,23 @@
+using AdventOfCode2022;
+
 namespace AdventOfCode2022
 {
     public abstract class Day
     {
-        protected readonly InputReader InputReader;
+        private readonly InputReader InputReader;
 
         protected Day()
         {
             InputReader = new InputReader(GetType().Name);
         }
 
-        protected abstract void LineAction(InputTextLine line);
+        protected abstract void LineAction(Token line);
         public int Result1 { get; protected set; } = 0;
         public int Result2 { get; protected set; } = 0;
 
         public virtual void Execute()
         {
-            foreach (InputTextLine line in InputReader)
+            foreach (Token line in InputReader)
                 LineAction(line);
         }
     }
--- AdventOfCode2022/AdventOfCode2022/Day3.cs
+++ AdventOfCode2022/AdventOfCode2022/Day3.cs
@@ -31,7 +31,7 @@ Uppercase item types A through Z have priorities 27 through 52.
 In the above example, the priority of the item type that appears in both compartments of each rucksack is 16 (p), 38 (L), 42 (P), 22 (v), 20 (t), and 19 (s); the sum of these is 157.
 
 Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?
-     */
+*/
 
 /*
 As you finish identifying the misplaced items, the Elves come to you with another issue.
@@ -92,7 +92,7 @@ public class Day3 : Day
         return common;
     }
 
-    protected override void LineAction(InputTextLine line)
+    protected override void LineAction(Token line)
     {
         var l = line.StringValue.Length / 2;
         var match = FindCommon(line.StringValue[..l], line.StringValue[l..]);
