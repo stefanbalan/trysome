1141d302ed098f58b35ac176547dc90d8375076f
Stefan Balan <stefan.balan@gmail.com>
2023-01-25 19:59:38 +0200

    start LAZY project, working html editor

---
+++ Lazy/Lazy/Client/Program.cs
@@ -0,0 +1,15 @@
+using Lazy.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+
+await builder.Build().RunAsync();
