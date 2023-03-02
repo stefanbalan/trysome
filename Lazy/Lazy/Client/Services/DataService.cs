bc9468513b2b5330c68715f4cb13173d70a47877
Stefan Balan <stefan.balan@gmail.com>
2023-03-02 22:02:55 +0200

    LAZY all crud working for EmailTemplate

--- Lazy/Lazy/Client/Services/DataService.cs
+++ Lazy/Lazy/Client/Services/DataService.cs
@@ -1,62 +1,72 @@
 ﻿using System.Net.Http.Json;
-using Lazy.Model;
 
-namespace Lazy.Client.Services
+namespace Lazy.Client.Services;
+
+public abstract class DataService<T>
 {
-    public abstract class DataService<T>
+    protected readonly HttpClient HttpClient;
+    protected abstract string ApiUrl { get; }
+
+    protected DataService(HttpClient httpClient)
     {
-        protected readonly HttpClient HttpClient;
-        protected abstract string ApiUrl { get; }
+        HttpClient = httpClient;
+    }
 
-        protected DataService(HttpClient httpClient)
+    public async Task<T?> GetById(int id)
+    {
+        try
         {
-            HttpClient = httpClient;
+            var result = await HttpClient.GetFromJsonAsync<T>($"{ApiUrl}/{id}");
+            Console.WriteLine($"DataService<{typeof(T).Name}> GET id {((dynamic)result).Id}");
+            return result;
         }
-
-        public async Task<T?> GetById(int id)
+        catch (Exception e)
         {
-            try
-            {
-                var result = await HttpClient.GetFromJsonAsync<T>($"{ApiUrl}/{id}");
-                return result;
-            }
-            catch (Exception e)
-            {
-                //todo show toast message and stop navigation
-                return default(T);
-            }
+            Console.WriteLine($"DataService<{typeof(T).Name}> GET exception [{e.Message}");
+            return default(T);
         }
+    }
 
-        public async Task<T?> CreateOrUpdate(T model)
+    public async Task<T?> CreateOrUpdate(T model)
+    {
+        // HttpClient.PutAsync("/api/EmailTemplate", new StringContent())
+        T? result = default;
+        try
         {
-            // HttpClient.PutAsync("/api/EmailTemplate", new StringContent())
             var response = await HttpClient.PutAsJsonAsync(ApiUrl, model);
-            T? result = default;
+
             if (response.IsSuccessStatusCode)
             {
                 result = await response.Content.ReadFromJsonAsync<T>();
                 if (result == null) throw (new Exception("Could not deserialize response"));
-                Console.WriteLine($"EmailTemplate PUT with id {((dynamic)result).Id}, [{response.StatusCode} - {response.ReasonPhrase}]");
+                Console.WriteLine($"DataService<{typeof(T).Name}> PUT with id {((dynamic)result).Id}, [{response.StatusCode} - {response.ReasonPhrase}]");
             }
             else
             {
                 var contentstring = await response.Content.ReadAsStringAsync();
-                Console.WriteLine($"EmailTemplate PUT error [{response.StatusCode} - {response.ReasonPhrase}] : {contentstring}");
+                Console.WriteLine($"DataService<{typeof(T).Name}> PUT error [{response.StatusCode} - {response.ReasonPhrase}] : {contentstring}");
             }
 
-            return result;
         }
-
-        public async Task DeleteById(int id)
+        catch (Exception e)
         {
-            try
-            {
-                await HttpClient.DeleteAsync($"{ApiUrl}/{id}");
-            }
-            catch (Exception e)
-            {
-                //todo show toast message and stop navigation
-            }
+            Console.WriteLine($"DataService<{typeof(T).Name}> PUT exception [{e.Message}");
+        }
+        return result;
+    }
+
+    public async Task<bool> DeleteById(int id)
+    {
+        try
+        {
+            var response = await HttpClient.DeleteAsync($"{ApiUrl}/{id}");
+            Console.WriteLine($"DataService<{typeof(T).Name}> DELETE [{response.StatusCode} - {response.ReasonPhrase}]");
+            return response.IsSuccessStatusCode;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"DataService<{typeof(T).Name}> DELETE exception [{e.Message}");
+            return false;
         }
     }
 }
