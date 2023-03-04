ec17ee8023b39720cc3ee8a8dc32c0e71216c4f6
Balan Stefan <stefan.balan@gmail.com>
2023-03-04 16:35:50 +0200

    LAZY add title to email template

---
+++ Lazy/Lazy.EF/Migrations/20230304131813_2_EmailTemplates.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lazy.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class _2_EmailTemplates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "EmailTemplates",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Title",
+                value: "Test email title");
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Title",
+                value: "Test email title");
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Title",
+                value: "Test email title");
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Title",
+                value: "Test email title");
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Title",
+                value: "Test email title");
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Title",
+                value: "Test email title");
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Title",
+                value: "Test email title");
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Title",
+                value: "Test email title");
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Title",
+                value: "Test email title");
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Title",
+                value: "Test email title");
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "Title",
+                value: "Test email title");
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "Title",
+                value: "Test email title");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "EmailTemplates");
+        }
+    }
+}
