782ca0337335204f904c02d5905f2e161ae68b6e
Stefan Balan <stefan.balan@gmail.com>
2023-03-11 09:30:55 +0200

    LAZY minor chanages

---
+++ Lazy/Lazy/Client/Pages/EmailTemplate/Edit.razor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Lazy.Model;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Radzen;
+
+namespace Lazy.Client.Pages.EmailTemplate;
+
+public partial class Edit
+{
+
+    [Parameter]
+    public int? TemplateId { get; set; }
+
+
+    private readonly EmailTemplateModel _defaultEmailTemplateModel = new() { Html = true };
+    private EmailTemplateModel _emailTemplate = new() { Html = true };
+
+    protected override void OnInitialized()
+    {
+        //EditContext = new EditContext(person);
+        //EditContext.OnFieldChanged += EditContext_OnFieldChanged;
+
+        base.OnInitialized();
+    }
+
+    protected override async Task OnParametersSetAsync()
+    {
+        if (TemplateId is null or 0)
+            _emailTemplate = new EmailTemplateModel { Html = true };
+        else
+            _emailTemplate = await DataService.GetById(TemplateId.Value) ?? _defaultEmailTemplateModel;
+    }
+
+    private void GoBack()
+    {
+        NavigationManager.NavigateTo("/EmailTemplate/List");
+    }
+
+    private async Task Save()
+    {
+        if (!_emailTemplate.IsValid())
+        {
+            NotificationService.Notify(new NotificationMessage { Severity = NotificationSeverity.Warning, Detail = "Email template model is not valid." });
+            return;
+        }
+        var result = await DataService.CreateOrUpdate(_emailTemplate);
+        if (result != null) _emailTemplate = result;
+    }
+
+
+    private void EditContext_OnFieldChanged()
+    {
+        // _hasChanges = true;
+    }
+}
---
+++ Lazy/Lazy/Client/Shared/BaseEditPage.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Lazy.Client.Shared;
+
+public class BaseEditPage : ComponentBase
+{
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = null!;
+
+    public bool HasChanges { get; set; }
+}
