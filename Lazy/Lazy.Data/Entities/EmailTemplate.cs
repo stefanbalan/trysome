e6ded39815e0af5ac9f1e3b6da9bb48ccead7e9a
Stefan Balan <stefan.balan@gmail.com>
2023-03-18 08:58:32 +0200

    LAZY misc

--- Lazy/Lazy.Data/Entities/EmailTemplate.cs
+++ Lazy/Lazy.Data/Entities/EmailTemplate.cs
@@ -6,6 +6,5 @@
         public string Name { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
         public string Text { get; set; } = string.Empty;
-        public bool Html { get; set; }
     }
 }
