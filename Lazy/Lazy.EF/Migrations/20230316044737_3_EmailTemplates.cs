dc428a3fac7aec70587c8aae9e76030898de3c30
Eugen Stefan BALAN <x170375>
2023-03-18 08:58:32 +0200

    LAZY misc

---
+++ Lazy/Lazy.EF/Migrations/20230316044737_3_EmailTemplates.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lazy.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class _3_EmailTemplates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Html",
+                table: "EmailTemplates");
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "Name",
+                value: "Template 12 acd");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Html",
+                table: "EmailTemplates",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Html",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Html",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Html",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Html",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Html",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Html",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Html",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Html",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Html",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Html",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "Html",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "EmailTemplates",
+                keyColumn: "Id",
+                keyValue: 12,
+                columns: new[] { "Html", "Name" },
+                values: new object[] { true, "Template 11 acd" });
+        }
+    }
+}
